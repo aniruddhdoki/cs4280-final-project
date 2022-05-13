af23428eea83145c4d6ead181628b323e487cb16
Aniruddh Doki <doki.aniruddh@gmail.com>
2022-05-13 15:29:53 -0400

    Update dataArrayList.java

--- dataArrayList.java
+++ dataArrayList.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+import org.json.JSONObject;
+
 public class dataArrayList {
   public static void main(String[] args) {
 
   }
-  
-  
+
+  public static ArrayList<T> parse(JSONObject json) {
+
+  }
+
 
 }
