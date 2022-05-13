29c8aa98ada6b51340a174ae1ceaff84f072a4eb
holiu365 <holiu365@gmail.com>
2022-05-13 15:55:59 -0400

    added the class <Course> and attempted to alter the searchbar and choicebox to the leftside

--- course_catalog.java
+++ course_catalog.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
@@ -15,13 +16,14 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class course_catalog extends Application{
     private TableView<Course> table = new TableView<Course>();
-    private final ObservableList<Person> data = FXCollections.observableArrayList(
+    private final ObservableList<Course> data = FXCollections.observableArrayList(
         new Course("TEST123", "Test Course", "Test Description")
     );
 
@@ -34,19 +36,16 @@ public class course_catalog extends Application{
     public void start(Stage stage){
         //Creating the GridPane and Setting up the Columns for the ListView
         GridPane gridPane = new GridPane();
+        Scene scene = new Scene(gridPane, 500, 500);
         stage.setTitle("College Course Catalog");
         stage.setWidth(500);
         stage.setHeight(500);
         final Label label = new Label("University of North Carolina - Chapel Hill Courses");
         table.setEditable(true);
-        TableColumn courseName = new TableColumn("Course Name");
-        TableColumn courseCode = new TableColumn("Course Code");
-        TableColumn descrip = new TableColumn("Description");
-        table.getColumns().addAll(courseCode, courseName, descrip);
-        final VBox vbox = new VBox();
-        vbox.setSpacing(5);
-        vbox.setPadding(new Insets(10, 0, 0, 10));
-        vbox.getChildren().addAll(label, table);
+        TableColumn courseNameCol = new TableColumn("Course Name");
+        TableColumn courseCodeCol = new TableColumn("Course Code");
+        TableColumn descripCol = new TableColumn("Description");
+        table.getColumns().addAll(courseCodeCol, courseNameCol, descripCol);
         gridPane.add(table, 1, 0, 3, 1);
         
         //Creating a filtered list with the data
@@ -65,22 +64,95 @@ public class course_catalog extends Application{
             switch (choiceBox.getValue())//Switch on choiceBox value
             {
                 case "Course Name":
-                    flPerson.setPredicate(p -> p.getFirstName().toLowerCase().contains(newValue.toLowerCase().trim()));//filter table by Course Name
+                    flCourse.setPredicate(p -> p.getCourseName().toLowerCase().contains(newValue.toLowerCase().trim()));//filter table by Course Name
                     break;
                 case "Course Code":
-                    flPerson.setPredicate(p -> p.getLastName().toLowerCase().contains(newValue.toLowerCase().trim()));//filter table by Course Code
+                    flCourse.setPredicate(p -> p.getCourseCode().toLowerCase().contains(newValue.toLowerCase().trim()));//filter table by Course Code
                     break;
                 case "Description":
-                    flPerson.setPredicate(p -> p.getEmail().toLowerCase().contains(newValue.toLowerCase().trim()));//filter table by email
+                    flCourse.setPredicate(p -> p.getDescrip().toLowerCase().contains(newValue.toLowerCase().trim()));//filter table by description words
                     break;
             }
         });
 
+        choiceBox.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal)
+                -> {//reset table and textfield when new choice is selected
+            if (newVal != null) {
+                searchBar.setText("");
+            }
+        });
 
-        Scene scene = new Scene(gridPane, 500, 500);
+        HBox hBox = new HBox(choiceBox, searchBar);//Add choiceBox and searchBar to hBox
+        hBox.setAlignment(Pos.CENTER);//Center HBox
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(label, table, hBox);
+
+        ((Group) scene.getRoot()).getChildren().addAll(vbox);
+
         stage.setScene(scene);
         stage.show();
 
         stage.show();
+    }
+
+    public static class Course
+    {
+        private final SimpleStringProperty courseName = new SimpleStringProperty();
+        private final SimpleStringProperty courseCode = new SimpleStringProperty();
+        private final SimpleStringProperty descrip = new SimpleStringProperty();
+
+        private Course(String cName, String cCode, String cDescrip)
+        {
+            this.courseName.setValue(cName);
+            this.courseCode.setValue(cCode);
+            this.descrip.setValue(cDescrip);
+        }
+
+        public String getCourseName()
+        {
+            return courseName.get();
+        }
+
+        public void setCourseName(String cName)
+        {
+            courseName.set(cName);
+        }
+        
+        public SimpleStringProperty getCourseNameProperty()
+        {
+            return courseName;
+        }
+        
+        public String getCourseCode()
+        {
+            return courseCode.get();
+        }
+
+        public void setCourseCode(String cCode)
+        {
+            courseCode.set(cCode);
+        }
+
+        public SimpleStringProperty getCourseCodeProperty()
+        {
+            return courseCode;
+        }
+        
+        public String getDescrip()
+        {
+            return descrip.get();
+        }
+
+        public void setDescrip(String cDescrip)
+        {
+            descrip.set(cDescrip);
+        }
+        
+        public SimpleStringProperty getDescripProperty()
+        {
+            return descrip;
+        }
     }
 }
