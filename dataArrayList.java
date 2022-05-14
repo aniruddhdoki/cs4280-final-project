b86e10fa3c03247bc0fa65ad73a95fe54ebb1909
Aniruddh Doki <doki.aniruddh@gmail.com>
2022-05-13 20:35:07 -0400

    completed the json parser
    created a class that could parse a json file and return an arraylist of course objects

--- dataArrayList.java
+++ dataArrayList.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 import java.nio.file.*;
 import java.util.regex.*;
 
@@ -10,24 +11,47 @@ public class dataArrayList {
     return data;
   }
 
-  public static String[] getArray(String data) throws Exception {
+  public static ArrayList<Course> getArray(String data) throws Exception {
+    ArrayList<Course> output = new ArrayList<>()
     data = data.substring(1, data.length() - 1).toString();
     String[] splitData = data.split("},");
+    try {
+      for (String course : splitData) {
+        String[] splitCourse = course.split("\",");
+        ArrayList<String> args = new ArrayList<>();
+        try {
+          for (String attribute : splitCourse) {
+            String[] isolateVal = attribute.split(":");
+            attribute = isolateVal[1];
+            if (attribute.charAt(0) == '"') {
+              attribute = attribute.substring(1);
+            }
+            if (attribute.charAt(attribute.length() - 1) == '"') {
+              attribute = attribute.substring(0, attribute.length() - 1);
+            }
 
-    for (String course : splitData) {
-      String[] splitCourse = course.split("\",");
-      for (String attribute : splitCourse) {
-        System.out.println(attribute);
+            args.add(attribute);
+          }
+        } catch (Exception e) {
+          System.err.println(e.toString());
+        }
+        try {
+          for (String arg : args) {
+            output.add(Course(args.get(2), args.get(1), args.get(4)));
+          }
+        } catch (Exception e) {
+          System.err.println(e.toString());
+        }
       }
+    } catch (Exception e) {
+      System.err.println(e.toString());
     }
-    return splitData;
+    return output;
   }
 
   public static void main(String[] args) throws Exception {
     String data = readFileAsString("data/data.json");
-    // System.out.println(data);
     String[] splitData = getArray(data);
-    
   }
 
 }
